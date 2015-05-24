ad967827473e563071327805c35c9ce171c82641
Ben Huson <j.ben.huson@gmail.com>
2015-05-24 17:19:04 -0400

    initial full moon watchface

--- src/moonface.c
+++ src/moonface.c
@@ -1,60 +1,97 @@
 #include <pebble.h>
 
+typedef struct {
+  int hours;
+  int minutes;
+} Time;
+
 static Window *window;
-static TextLayer *text_layer;
+static Layer *canvas_layer;
+static Time last_time;
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Select");
+// on tick handler
+static void tick_handler(struct tm *tick_time, TimeUnits changed) {
+  // Store time
+  last_time.hours = tick_time->tm_hour;
+  last_time.hours -= (last_time.hours > 10) ? 12 : 0;
+  last_time.minutes = tick_time->tm_min;
+
+  // Redraw
+  if(canvas_layer) {
+    layer_mark_dirty(canvas_layer);
+  }
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Up");
+// setup our main canvas
+static void canvas_update_proc(Layer *this_layer, GContext *ctx) {
+    GRect bounds = layer_get_bounds(this_layer);
+
+    // set background
+    graphics_fill_rect(ctx, GRect(0, 0, bounds.size.w, bounds.size.h), 0, GColorBlack);
+
+    // Get the center of the screen (non full-screen)
+    GPoint center = GPoint(bounds.size.w / 2, (bounds.size.h / 2));
+
+    // make our moon circle in our canvas layer
+    graphics_context_set_fill_color(ctx, GColorWhite);
+    graphics_fill_circle(ctx, center, 65);
+
 }
 
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  text_layer_set_text(text_layer, "Down");
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
+// setup our main window on load
 static void window_load(Window *window) {
-  Layer *window_layer = window_get_root_layer(window);
-  GRect bounds = layer_get_bounds(window_layer);
+    // get the window root layer
+    Layer *window_layer = window_get_root_layer(window);
+    // find the bounds of the window layer
+    GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
-  text_layer_set_text(text_layer, "Press a button");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
-  layer_add_child(window_layer, text_layer_get_layer(text_layer));
+    // Create Layer
+    canvas_layer = layer_create(GRect(0, 0, bounds.size.w, bounds.size.h));
+    // add the canvas to the window as a child
+    layer_add_child(window_layer, canvas_layer);
+    // Set the update_proc
+    layer_set_update_proc(canvas_layer, canvas_update_proc);
 }
 
 static void window_unload(Window *window) {
-  text_layer_destroy(text_layer);
+    // remove our canvas_layer from the window on unload.
+    layer_destroy(canvas_layer);
 }
 
 static void init(void) {
-  window = window_create();
-  window_set_click_config_provider(window, click_config_provider);
-  window_set_window_handlers(window, (WindowHandlers) {
-    .load = window_load,
-    .unload = window_unload,
-  });
-  const bool animated = true;
-  window_stack_push(window, animated);
+    srand(time(NULL));
+
+    // create a time from the localtime at initialization
+    time_t t = time(NULL);
+    struct tm *time_now = localtime(&t);
+    // set our tick handler to have the current time
+    tick_handler(time_now, MINUTE_UNIT);
+
+    // create our "window" on the watch
+    window = window_create();
+    // setup our window handler
+    window_set_window_handlers(window, (WindowHandlers) {
+        .load = window_load,
+        .unload = window_unload,
+    });
+    // setup a const animated to show we want the window animated.
+    const bool animated = true;
+    // push the window on the window stack
+    window_stack_push(window, animated);
 }
 
 static void deinit(void) {
-  window_destroy(window);
+    //destroy our window
+    window_destroy(window);
 }
 
 int main(void) {
-  init();
-
-  APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
-
-  app_event_loop();
-  deinit();
+    // initialize the app
+    init();
+    // log that we are loaded on the window
+    APP_LOG(APP_LOG_LEVEL_DEBUG, "Done initializing, pushed window: %p", window);
+    // start the application event loop (standard pebble)
+    app_event_loop();
+    // deinitialize the application
+    deinit();
 }
